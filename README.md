56aa66f5d3fb55104aabc04bb3c5465eb27939ff
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-08-23 17:56:19 -0300

    It is generated the list of song from the data source

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ README.md
 ### Instructor: Petriz Celaya.
 * Description: It allows listening to music from a list of songs uploaded previously.
 
-#### Tools:
+### Tools:
 * Google Fonts: https://fonts.google.com/
 * Cortar audio online: http://mp3cut.net/es/
 * Font Awesome: http://fontawesome.io/
--- js/app.js
+++ js/app.js
@@ -2,11 +2,22 @@ $(document).ready(function(){
   getSongs();
 });
 
-var audio;
-var music;
+var audio, music;
 
 function getSongs(){
   $.getJSON("js/app.json", function(mjson){
     music = mjson;
+    genList(music); //It generates the song list
+  });
+};
+
+function genList(music) {
+  console.log(music.songs);
+  $.each(music.songs, function(i, song){
+    $('#playlist').append('<li class="songs-li" id="' + i + '">' + song.name + '</li>');
+  }); //It adds for each song a li tag
+  $('#playlist li').click(function(){ //It'll play the selected song when the user clicks a name song
+    var selectedSong = $(this).attr('id');
+    console.log(selectedSong);
   });
 };
