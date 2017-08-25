9129e7f423cc4dadedd250f39e43c0b7872abe27
Lucia Calisaya <calisaya.lucia@gmail.com>
2017-08-25 11:27:54 -0300

    Mix the playlist of songs when a button is clicked

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
+  initPlayer();
   getSongs();
 });
 
 var audio = document.getElementById('player');
 var music;
+
+function initPlayer(){
+  $('#shuffle').click(function(){
+    $('#playlist'),empty();
+    console.log(shuffle(music.songs));
+    genList(music);
+    playSong(0); //when shuffle button is clicked, the playlist is regenerated with a different order and it will be play the first song
+  });
+};
 
 function getSongs(){
   $.getJSON("js/app.json", function(mjson){
@@ -28,7 +38,7 @@ function playSong(id){
   console.log(id);
   var upperBound = music.songs;
   if (id >= upperBound.length){ //when it was reached the last song the audio puts on in pause
-    console.log('Se acabó la canción');
+    console.log('Se acabó la música');
     audio.pause();
   } else {
     $('#img-album').attr('src', music.songs[id].img);
@@ -40,8 +50,18 @@ function playSong(id){
 };
 
 function scheduleSong(id){
-  audio.onended = function () { //at the moment of ended a song, it'll be reproduced the next song 
+  audio.onended = function () { //at the moment of ended a song, it'll be reproduced the next song
     console.log('Terminó la canción');
     playSong(parseInt(id)+1);
   }
 };
+
+function shuffle(array){
+  for(var random, temp, position=array.length; //It's a cycle, it's declared all the variables to use
+    position;
+    random=Math.floor(Math.random()*position), //It's generated a random number
+    temp=array[--position], //it's saved temporally the song in the array -1 position
+    array[position]=array[random], //In the array position puts some random song
+    array[random]=temp); //And in that random position is assigned this current array position
+  return array;
+}
